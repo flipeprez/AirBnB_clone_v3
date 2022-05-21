92f437444e9378a6fdc310a04980fe8933c02777
camia97 <3962@holbertonschool.com>
2022-05-21 19:23:43 -0300

    pycodestyle

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,7 +6,6 @@ from flask import Flask
 import os
 
 
-
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
@@ -16,5 +15,7 @@ def close(a):
     """Handle the app.teardown_appcontext"""
     return storage.close()
 
+
 if __name__ == "__main__":
-    app.run(host=os.getenv('HBNB_API_HOST'), port=os.getenv('HBNB_API_PORT'), threaded=True)
+    app.run(host=os.getenv('HBNB_API_HOST'),
+            port=os.getenv('HBNB_API_PORT'), threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,6 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-
 @app_views.route('/status')
 def dict_json():
     """return json"""
