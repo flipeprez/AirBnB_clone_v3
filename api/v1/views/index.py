5b930750555b911c0a1e7ae8b7ce807e2e723bec
camia97 <3962@holbertonschool.com>
2022-05-22 22:20:13 -0300

    add task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 """Modulo index"""
+from models import storage
+from models.state import State
 from api.v1.views import app_views
 from flask import jsonify
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.user import User
 
 
 @app_views.route('/status')
@@ -9,5 +16,18 @@ def dict_json():
     """return json"""
     my_dict = {
         "status": "OK"
+    }
+    return jsonify(my_dict)
+
+@app_views.route('/stats')
+def numb_obj():
+    """endpoint that retrives number of each object"""
+    my_dict = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
     }
     return jsonify(my_dict)
