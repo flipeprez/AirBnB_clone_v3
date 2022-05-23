598ecbde3afb7ea439c7306de1edab9da224addd
camia97 <3962@holbertonschool.com>
2022-05-23 17:58:20 -0300

    add task 7

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
 '''a nice comment'''
-from api.v1.view import app_views
+from api.v1.views import app_views
 from flask import Flask, jsonify, request
 from models import storage
 from models.state import State
 
 
-@app_views('/states/', method=['GET'], strict_slashes=False)
+@app_views.route('/states', strict_slashes=False)
 def state():
-    if request.method == 'GET'
+    print("a")
+    if request.method == 'GET':
         ls = []
-        for key in storage.all(State).value():
-            ls.append(key.to_dict())
+        for obj in storage.all(State).values():
+            ls.append(obj.to_dict())
         return jsonify(ls)
-    '''
+
+
+'''
     if request.method == 'POST'
 	n_state = storage.new(State)
 	request.get_json('n_state')
