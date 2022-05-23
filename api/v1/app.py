cd3f65b02115f4d0dc6e4a1d9832076d876c39b4
camia97 <3962@holbertonschool.com>
2022-05-22 22:44:08 -0300

    add task 6

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """Status of your API"""
-from http.client import HTTPException
-from urllib import response
 from api.v1.views import app_views
 from models import storage
 from flask import Flask, jsonify
