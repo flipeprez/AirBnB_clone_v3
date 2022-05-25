bd2ffdc5bcb73bc28ce642a1e9d13795976c82d6
flipeprez <flipeprez@gmail.com>
2022-05-25 02:29:15 +0000

    fix pep 8

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -20,7 +20,8 @@ def state(state_id=None):
         return jsonify(mstates.to_dict())
     abort(404)
 
-@app_views.route('/states/<states_id>', methods=['DELETE'], strict_slashes=False)
+
+@app_views.route('/states/<s_id>', methods=['DELETE'], strict_slashes=False)
 def del_states(s_id):
     '''a nice comment'''
     gtstates = storage.get("states", s_id)
@@ -29,6 +30,7 @@ def del_states(s_id):
     storage.delete(gtstates)
     storage.save()
     jsonify({})
+
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def state_pos():
@@ -45,8 +47,9 @@ def state_pos():
 
     return (jsonify(n_state.to_dict()), 201)
 
+
 @app_views.route('/states/<states_id>', methods=['PUT'], strict_slashes=False)
-def update_states(states_id)
+def update_states(states_id):
     '''a nice comment'''
     gtcont = request.get_json()
     if gtcont is None:
@@ -59,6 +62,6 @@ def update_states(states_id)
     n_allowed = ["id", "created_at", "updated_at"]
     for key, value in gtcont.items():
         if key not in n_allowed:
-           setattr(upstate, key, value)
+            setattr(upstate, key, value)
         upstate.save()
         return jsonify(upstate.to_dict())
